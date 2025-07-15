b63faec3c8c82e2eec6a0631d8f49fd20e40081b
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-15 16:09:33 +0200

    update component

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,5 +1,13 @@
-const Items = () => {
-  return <div>AddItem</div>;
+import SingleItem from './SingleItem';
+
+const Items = ({ items, removeItem }) => {
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return <SingleItem key={item.id} item={item} removeItem={removeItem} />;
+      })}
+    </div>
+  );
 };
 
 export default Items;
